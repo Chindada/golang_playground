e46e83e3d848437ac851747a02e5ae015226b4f1
TimHsu@M1BP-20210907 <maochindada@gmail.com>
2021-10-14 14:40:29 +0800

    test git push at vscode

---
+++ 211001/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a := []int{1, 2, 3, 4, 5}
+	// b := a[:(len(a)-1)/2]
+	// c := a[(len(a)-1)/2:]
+	// fmt.Println(b)
+	// fmt.Println(c)
+	a := 1
+	switch a {
+	case 1:
+		fmt.Println("a")
+	case 2:
+		fmt.Println("b")
+	}
+}
